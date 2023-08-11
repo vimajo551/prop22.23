fedea98788fd7a5979d4244bcbfbd58593379cc2
vimajo551 <104533881+vimajo551@users.noreply.github.com>
2023-08-11 20:07:03 -0300

    Add files via upload

--- js/PlayerArcher.js
+++ js/PlayerArcher.js
@@ -11,20 +11,21 @@ class PlayerArcher {
     this.image = loadImage("./assets/playerArcher.png");
 
     World.add(world, this.body);
-    Matter.Body.setAngle(this.body, -90); 
+
+    Matter.Body.setAngle(this.body, -PI / 2); // -90 degree
   }
 
   display() {
     var pos = this.body.position;
     var angle = this.body.angle;
 
-    if (keyIsDown(DOWN_ARROW) && angle < -73 ) {
-      angle += 1;
+    if (keyIsDown(DOWN_ARROW) && angle < -1.2) {
+      angle += 0.01;
       Matter.Body.setAngle(this.body, angle);
     }
 
-    if (keyIsDown(UP_ARROW) && angle > -103) {
-      angle -=1;
+    if (keyIsDown(UP_ARROW) && angle > -1.6) {
+      angle -= 0.01;
       Matter.Body.setAngle(this.body, angle);
     }
 
--- js/PlayerArrow.js
+++ js/PlayerArrow.js
@@ -9,19 +9,17 @@ class PlayerArrow {
     this.body = Bodies.rectangle(x, y, this.width, this.height, options);
     this.image = loadImage("./assets/arrow.png");
     this.archerAngle = archerAngle;
-    this.velocity = 0;
+    this.velocity = p5.Vector.fromAngle(archerAngle);
     World.add(world, this.body);
   }
 
   shoot(archerAngle) {
-    archerAngle += 90;
-    this.velocity = p5.Vector.fromAngle(archerAngle * (3.14 / 180));
-
-    this.velocity.mult(0.5);
+    this.velocity = p5.Vector.fromAngle(archerAngle + PI / 2);
+    this.velocity.mult(55);
 
     Matter.Body.setVelocity(this.body, {
-      x: this.velocity.x * (180 / 3.14),
-      y: this.velocity.y * (180 / 3.14)
+      x: this.velocity.x,
+      y: this.velocity.y
     });
 
     Matter.Body.setStatic(this.body, false);
@@ -30,10 +28,9 @@ class PlayerArrow {
   display() {
     var tmpAngle;
     if (this.body.velocity.y === 0) {
-      tmpAngle = this.archerAngle + 90;
+      tmpAngle = this.archerAngle + PI / 2;
     } else {
-      tmpAngle =
-        Math.atan(this.body.velocity.y / this.body.velocity.x) * (180 / 3.14);
+      tmpAngle = Math.atan(this.body.velocity.y / this.body.velocity.x);
     }
 
     Matter.Body.setAngle(this.body, tmpAngle);
